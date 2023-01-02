ab895d10f58dff18ab0b866e4ef53a579751c5d7
DuckMcQuacks <menus.dominik@gmail.com>
2023-01-02 15:58:41 +0100

    Worked on navbar

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'
+import { Route, Routes} from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
 
 export default function App() {
   return (
@@ -7,6 +12,14 @@ export default function App() {
     <header>
       <Navbar />
     </header>
+    <div className='container'> 
+      <Routes>
+        <Route path ="/" element={<Home />}/>
+        <Route path ="/About" element={<About />}/>
+        <Route path ="/Projects" element={<Projects />}/>
+        <Route path ="/Contact" element={<Contact />}/>
+      </Routes>
+    </div>
   </>
   )
 }
--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -1,9 +1,21 @@
 import React from "react"
+import { Link} from "react-router-dom"
 
 export default function Navbar(){
     return(
         <nav>
-            <button className ="navButton"> Click me!</button>
+            <Link to="/" >
+                <p className="navButton">Home</p>
+            </Link>
+            <Link to="/About" >
+            <p className="navButton">About</p>
+            </Link>
+            <Link to="/Projects" >
+            <p className="navButton">Projects</p>
+            </Link>
+            <Link to="/Contact" >
+            <p className="navButton">Contact</p>
+            </Link>
         </nav>
     )
 }
