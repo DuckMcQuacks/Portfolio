493394025031713832696840091b9ae2f0db0a26
DuckMcQuacks <menus.dominik@gmail.com>
2023-01-03 11:29:16 +0100

    Prototype sketch animation
    Just committing to have it saved for now

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -1,5 +1,11 @@
+import image from "../assets/bw-image.jpg"
+import imag from "../assets/color-image.jpg"
+
 export default function Home(){
     return(
-        <p>Home</p>
+        <div className="mask-container">
+            <div className="color-image"></div>
+            <img className="blackWhite" />
+        </div>
     )
 }
