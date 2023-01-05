90049f299c7cb451b974badb0d0a6952d9e53711
DuckMcQuacks <menus.dominik@gmail.com>
2023-01-05 15:35:53 +0100

    Skills
    Add the titles to all of the skills, when they are hovered over they will appear.

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -3,10 +3,30 @@ interface props{
 }
 
 export default function About({darkmode}:props){
-    const skills : string[] = ["HTML","CSS","JavaScript","TypeScript","React","GIT","Github","Responsive Design","Terminal","SQL Basics","C#","C","Python"];
-    const skillsArray = skills.map(a=>{
+    const skillsObject : skillsType = {
+        skills : ["HTML","CSS","JavaScript","TypeScript","React","GIT","Github","RWD","Terminal","SQL","C#","C","Python"],
+        skillsDesc : ["Hypertext markup language, is a markup language used to create the frontend of websites.",
+        "Cascading Style Sheets, used to manipulate the properties of HTML",
+        "A scripting or programming language that allows you to implement complex features on web pages",
+        "A superset of JavaScript, it's Object-oriented where JavaScript is a prototype based language.",
+        "A JavaScript-based UI development library, focused on making it easy to create single-page websites.",
+        "A version control system, it enables multiple developers to work on the same code base and tracks changes.",
+        "A code hosting platform for version control and collaboration. Makes it easy to share code between developers.",
+        "Responsive web design allows the page to adopt to different screen sizes and ratios, allowing the page to look great on all screens.",
+        "A text-based interface, the ability to efficiently navigate and run programs in a terminal.",
+        "Structured Query Language is used to communicate with a database, allowing to read and write it, used for relational databases.",
+        "A general purpose programming language, it's an object oriented programming language.",
+        "A low level general purpose programming language, allows more control over memory usage.",
+        "A high-level object oriented programming language, commonly used for data science and back-end"
+    ]
+    }
+    interface skillsType{
+        skills : string[]
+        skillsDesc : string[]
+    }
+    const skillsArray = skillsObject.skills.map((a,b)=>{
         return(
-            <div className={"aboutSkill" +(darkmode ? " aboutSkillDark" : "")}>{a}</div>
+            <div className={"aboutSkill" +(darkmode ? " aboutSkillDark" : "")} title={skillsObject.skillsDesc[b]}>{a}</div>
         )
     })
     return(
