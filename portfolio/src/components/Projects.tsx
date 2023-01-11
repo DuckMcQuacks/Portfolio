85607353324269dffadad8eda04bfa31a4aaf140
DuckMcQuacks <menus.dominik@gmail.com>
2023-01-11 17:10:07 +0100

    Fix public image
    Public image wasn't working tried to set it to relative to see if it works.

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -11,8 +11,8 @@ export default function Projects({darkmode}:props){
             <p>Here you can find some of the projects I have worked on before. <br /> Each has a title, a short description, and a link to a page where I go into more detail about the project. <br /> Each project has its own documentation as well.</p>
             </div>
             <ProjectTemplate
-            imageDark={"/images/portfolio_desktopDark.png"}
-            imageLight={"/images/portfolio_desktopLight.png"}
+            imageDark={"./images/portfolio_desktopDark.png"}
+            imageLight={"./images/portfolio_desktopLight.png"}
             darkMode={darkmode}
             desc={"My portfolio. A hub where I link my projects. For the layout I have taken inspiration from Ram Maheswari's portfolio. "}
             title={"Portfolio"}/>
