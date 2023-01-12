a6891f773cd767e707c895fefbf92945b886d881
DuckMcQuacks <menus.dominik@gmail.com>
2023-01-12 16:04:46 +0100

    Project template
    Added desktop and phone mode.

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -1,6 +1,8 @@
 import ProjectTemplate from "./projectTemplate"
-import portfolioDesktopDark from "./images/portfolio_desktopDark.png";
-import portfolioDesktopLight from "./images/portfolio_desktopLight.png";
+import portfolioDarkDesktop from "./images/portfolio_desktopDark.png";
+import portfolioDarkPhone from "./images/portfolio_phoneDark.jpg"
+import portfolioLightDesktop from "./images/portfolio_desktopLight.png";
+import portfolioLightPhone from "./images/portfolio_phoneLight.jpg"
 interface props {
     darkmode: boolean,
 }
@@ -13,8 +15,10 @@ export default function Projects({darkmode}:props){
             <p>Here you can find some of the projects I have worked on before. <br /> Each has a title, a short description, and a link to a page where I go into more detail about the project. <br /> Each project has its own documentation as well.</p>
             </div>
             <ProjectTemplate
-            imageDark={portfolioDesktopDark}
-            imageLight={portfolioDesktopLight}
+            imageDarkDesktop={portfolioDarkDesktop}
+            imageDarkPhone={portfolioDarkPhone}
+            imageLightDesktop={portfolioLightDesktop}
+            imageLightPhone={portfolioLightPhone}
             darkMode={darkmode}
             desc={"My portfolio. A hub where I link my projects. For the layout I have taken inspiration from Ram Maheswari's portfolio. "}
             title={"Portfolio"}/>
--- portfolio/src/components/projectTemplate.tsx
+++ portfolio/src/components/projectTemplate.tsx
@@ -1,17 +1,48 @@
 import {Link} from "react-router-dom"
+import {useState, useEffect} from 'react'
 
 interface props{
     darkMode: boolean,
-    imageDark: string,
-    imageLight: string,
+    imageDarkDesktop: string,
+    imageDarkPhone: string,
+    imageLightDesktop: string,
+    imageLightPhone: string,
     desc: string,
     title: string
 }
 
 export default function ProjectTemplate(props: props){
+    const [width , setWidth] = useState(window.innerWidth)
+  
+    //Conditional rendering for cross-Device compatibility regarding phones
+    useEffect(() => {
+        let cb = function () {
+          setWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", cb);
+      
+        return () => {
+          window.removeEventListener("resize", cb);
+        };
+      }, []);
+
+    function imageDecider(){
+        if(props.darkMode){
+            if(width > 720){
+                return props.imageDarkDesktop
+            }
+            return props.imageDarkPhone
+        }
+        if(width> 720){
+            return props.imageLightDesktop
+        }
+        return props.imageLightPhone
+    }
+
+    let image : string = imageDecider();
     return(
         <div className="projectTemplate">
-            <img className="projectImage" src={props.darkMode ? props.imageDark : props.imageLight }/>
+            <img className="projectImage" src={image}/>
             <div className="projectDesc">
                 <h1>{props.title}</h1>
                 {props.desc}
