80c49445b218a254a979eddc4593ebdfda57ba44
DuckMcQuacks <menus.dominik@gmail.com>
2023-01-16 15:46:12 +0100

    CaseStudy Portfolio
    Finished the case study of portfolio.

--- portfolio/src/components/PortfolioStudy.tsx
+++ portfolio/src/components/PortfolioStudy.tsx
@@ -39,6 +39,27 @@ export default function PortfolioStudy({darkmode} : props){
 
     let image : string = imageDecider();
     const isInvert= darkmode ? "" : " invert"
+    //Tools used and the description for each
+    const skillsObject : skillsType = {
+        skills : ["HTML","CSS","TypeScript","React","GIT","Github"],
+        skillsDesc : ["Hypertext markup language, is a markup language used to create the frontend of websites.",
+        "Cascading Style Sheets, used to manipulate the properties of HTML",
+        "A superset of JavaScript, it's Object-oriented whereas JavaScript is a prototype based language.",
+        "A JavaScript-based UI development library, focused on making it easy to create single-page websites.",
+        "A version control system, it enables multiple developers to work on the same code base and tracks changes.",
+        "A code hosting platform for version control and collaboration. Makes it easy to share code between developers.",
+    ]
+    }
+    interface skillsType{
+        skills : string[]
+        skillsDesc : string[]
+    }
+    const skillsArray = skillsObject.skills.map((a,b)=>{
+        return(
+            <div key={b} className={"aboutSkill" +(darkmode ? " aboutSkillDark" : "")} title={skillsObject.skillsDesc[b]}>{a}</div>
+        )
+    })
+
     return(
     <>
         <div className={"container pattern columnCenter"+(darkmode?"" : " patternGrey")}>
@@ -58,6 +79,13 @@ export default function PortfolioStudy({darkmode} : props){
         <div className="caseStudyContainer">
         <h1>Overview</h1>
         <p>Portfolio is a simple and clean single-page website. The websites is responsive, providing a clean and consistent look on all devices allowing for ease of use. It has a built-in theme switcher so that the user can choose wether they want a bright look and feel to the site, or a dark one.</p>
+        <p>I used a few tools, the very obvious ones are HTML and CSS. I used vite TypeScript-React framework for the Front-End. This was my first project that used TypeScript, it took a bit to get used to it, but overall it made it easier to develop the project. </p>
+        <p>I used GIT for version control and Github for storage, I was the sole developer that worked on the project. For the layout I took inspiration from <a href="https://www.rammaheshwari.com" target="_blank"> Ram Maheswari </a> as I very much liked the layout of his portfolio.</p>
+        <p>You can find a README.md in the github repository which you can look into if you want further information regarding the project.</p>
+        <h1>Tools Used</h1>
+        <div className="aboutSkills">
+                    {skillsArray}
+        </div>
         </div>
       </>  
     )
